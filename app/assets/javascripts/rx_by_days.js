ca168cb13596f0c8164e56ab07a4bf02f4ca81a8
chayarn20 <chayaa@3base.co.il>
2013-01-09 11:58:46 +0200

    change script js to plus minus

--- app/assets/javascripts/rx_by_days.js
+++ app/assets/javascripts/rx_by_days.js
@@ -1,34 +1,36 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
-// $(function () {
 $(document).ready( function (){
 
-$('.rxchange').live('click',function(){
+	$('.rxchange').live('click',function(){
+					var that = this;
+		 			var currentTr = $(this).closest("tr");
+		 			var rx = currentTr.children(':first-child').next().text();
+		    		var date1 = currentTr.children(':first-child').next().next().text();
+				    
+				    if(	$(this).attr('class') == "rxchange rxplus"){
+	          			
+	          			$.ajax({
+							type: "get",
+							url: "rx_by_days/rx_h",
+			   				data: "rx=" + rx + "&date1=" + date1,
+			  				success: function(data){
+			  					if (!(currentTr.next().hasClass('extendedRowTable')))
+			  					{
+			  						$(that).html("-");
+	 								$(that).addClass("rxminus");
+		 							$(that).removeClass("rxplus");
+			  						currentTr.after('<tr class="extendedRowTable"><td colspan=" ' + currentTr.children("td").length + '"></td></tr>')
+	            					currentTr.next().children().html(data);
+	            				}
+	            			}
+						});
+					}					
+			 		else 
+					{
+						$(that).removeClass("rxminus");
+		 				$(that).addClass("rxplus");
+						$(that).html("+");
+						currentTr.next().remove();
+					}
+	});	
 
-			 	$(this).toggleClass("rxminus");
-			    $(this).toggleClass("rxplus");
-			 	current_tr = $(this).closest("tr");
-		 		rx = current_tr.children(':first-child').next().text();
-			    date1 = current_tr.children(':first-child').next().next().text();
-				if($(this).attr('class')=="rxchange rxminus"){
-					$(this).html("-");
-					current_tr.after('<tr class="extendedRowTable"><td colspan=" '+ current_tr.children("td").length + '"></td></tr>')
-				    $.ajax({
-		  			    type: "get",
-						url: "rx_by_days/rx_h",
-						data: "rx="+rx+"&date1="+date1,
-						success: function(data){
-				            	current_tr.next().children().html(data);				            	
-				            }
-					});						
-				}
-				 else 
-				{
-					$(this).html("+");
-					current_tr.next().remove();
-				}
-			});	
-})
-
-
+});	
--- app/assets/javascripts/tx_by_days.js
+++ app/assets/javascripts/tx_by_days.js
@@ -1,30 +1,36 @@
-
 $(document).ready( function (){
-	
-$('.txchange').live('click',function(){
-				
-	 			$(this).toggleClass("txminus");
-	 			$(this).toggleClass("txplus");
-	 			current_tr = $(this).closest("tr");
- 				tx = current_tr.children(':first-child').next().text();
-	    		date1 = current_tr.children(':first-child').next().next().text();
-			    if($(this).attr('class')=="txchange txminus"){
-					$(this).html("-");
-	      			current_tr.after('<tr class="extendedRowTable"><td colspan=" '+ current_tr.children("td").length + '"></td></tr>')
-          			$.ajax({
-						type: "get",
-						url: "tx_by_days/tx_h",
-		   				data: "tx="+tx+"&date1="+date1,
-		  				success: function(data){
-            				current_tr.next().children().html(data);
-            	
-            			}
-					});		
-				}
-		 		else 
-				{
-					$(this).html("+");
-					current_tr.next().remove();
-				}
-			});	
+
+	$('.txchange').live('click',function(){
+					var that = this;
+		 			var currentTr = $(this).closest("tr");
+		 			var tx = currentTr.children(':first-child').next().text();
+		    		var date1 = currentTr.children(':first-child').next().next().text();
+				    
+				    if(	$(this).attr('class') == "txchange txplus"){
+	          			
+	          			$.ajax({
+							type: "get",
+							url: "tx_by_days/tx_h",
+			   				data: "tx=" + tx + "&date1=" + date1,
+			  				success: function(data){
+			  					if (!(currentTr.next().hasClass('extendedRowTable')))
+			  					{
+			  						$(that).html("-");
+	 								$(that).addClass("txminus");
+		 							$(that).removeClass("txplus");
+			  						currentTr.after('<tr class="extendedRowTable"><td colspan=" ' + currentTr.children("td").length + '"></td></tr>')
+	            					currentTr.next().children().html(data);
+	            				}
+	            			}
+						});
+					}					
+			 		else 
+					{
+						$(that).removeClass("txminus");
+		 				$(that).addClass("txplus");
+						$(that).html("+");
+						currentTr.next().remove();
+					}
+	});	
+
 });	
